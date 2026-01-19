e8240aa573778eae0fd9b693b0061b1befe276ec
ShaheerAliKhan <bloodreservation@gmail.com>
2026-01-19 16:17:29 +0500

    refactor: Remove WebRTCServiceDelegate protocol to streamline service implementation

--- Sources/WebRTCService.swift
+++ Sources/WebRTCService.swift
@@ -3,22 +3,6 @@ import WebRTC
 import SocketIO
 import AVFoundation
 
-// MARK: - Delegate
-
-public protocol WebRTCServiceDelegate: AnyObject {
-    func onConnectionChange(_ connected: Bool)
-    func onLocalStream(_ stream: RTCMediaStream)
-    func onRemoteStream(participantId: String, stream: RTCMediaStream)
-    func onRemoteStreamRemoved(participantId: String)
-    func onOnlineUsers(_ users: [[String: Any]])
-    func onIncomingCall(_ data: [String: Any])
-    func onCallAccepted(_ data: [String: Any])
-    func onCallRejected(_ data: [String: Any])
-    func onParticipantJoined(_ data: [String: Any])
-    func onParticipantLeft(_ data: [String: Any])
-    func onError(_ message: String)
-}
-
 // MARK: - Service
 
 public final class WebRTCService {
